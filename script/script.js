3bd2f681badb3954d8ba881943e6a12fa3c8c45a
Angela Ruecker <angelrck@gmail.com>
2026-03-10 22:37:01 +0100

    Ajout skills, about

--- script/script.js
+++ script/script.js
@@ -40,8 +40,9 @@ function createSkillsFromJSON() {
                 card.innerHTML = `
                     <div class="card skillsText">
                         <div class="card-body">
-                            <img src="./assets/images/${item.image}" />
+                            <img src="./assets/images/${item.image}" alt="${item.alt}">
                             <h3 class="card-title mt-3">${item.title}</h3>
+                            <span class="badge bg-primary mt-2">${item.categorie}</span>
                             <p class="card-text mt-3">${item.text}</p>
                         </div>
                     </div>
@@ -75,7 +76,7 @@ function createPortfolioFromJSON() {
                 card.classList.add("col-lg-4", "mt-4");
                 card.innerHTML = `
                     <div class="card portfolioContent">
-                    <img class="card-img-top" src="./assets/images/${item.image}" style="width:100%">
+                    <img class="card-img-top" src="./assets/images/${item.image}" style="width:100%" alt="${item.alt}">
                     <div class="card-body">
                         <h3 class="card-title">${item.title}</h3>
                         <p class="card-text">${item.text}</p>
